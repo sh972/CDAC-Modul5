48ba0f6dea00cc41013fb3943fb1a2577779029c
Shubham Kulthe <67493796+sh972@users.noreply.github.com>
2022-01-08 09:18:22 +0530

    Add files via upload

---
+++ cdac.sql
@@ -0,0 +1,7 @@
+show databases;
+create database practice1;
+use practice1;
+
+create table user(username varchar(50),password varchar(50));
+select * from user;
+insert into user values (1,"shubham");
